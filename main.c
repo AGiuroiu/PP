3276e1544674773cb10cd75fa5f31b195e24b781
AGiuroiu <agiurou@gmail.com>
2023-04-02 23:23:29 +0300

    scriere in fisier wanna be

--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 int main()
 {
     // structura necesara pentru salvarea incidentelor
-    // un  incident i  va fi format din orasIncidente[i], strazi[i],incidente[i]
+    // un incident i va fi format din orasIncidente[i], strazi[i],incidente[i]
     int orasIncidente[100];
     char *strazi[100];
     int incidente[100];
     int count = 0;
 
-    // Lista de orase a  aplicatiei
+    // Lista de orase a aplicatiei
     char *orase[3];
     int nrOrase = 3;
     orase[0] = "Timisoara";
@@ -26,34 +26,33 @@ int main()
     tipIncident[2] = "Cablu cazut pe carosabil";
 
     // date de test
-    //aici o sa facem citirea evenimentelor existente in fisier
-     FILE* file = fopen("incidente.txt", "r");\
+    // aici o sa facem citirea evenimentelor existente in fisier
+    FILE *file = fopen("incidente.txt", "r");
 
-     char *currentline;
+    char *currentline;
 
     currentline = (char *)malloc(50 * sizeof(char));
-     size_t len=0;
-     int indiceLinie=0;
-     while (  fgets(currentline,50, file) != NULL) {
+    size_t len = 0;
+    int indiceLinie = 0;
+    while (fgets(currentline, 50, file) != NULL)
+    {
 
-
-        if (indiceLinie%3==0)
+        if (indiceLinie % 3 == 0)
         {
-            orasIncidente[indiceLinie/3]= atoi(currentline);
+            orasIncidente[indiceLinie / 3] = atoi(currentline);
         }
-        if (indiceLinie%3==1)
+        if (indiceLinie % 3 == 1)
         {
-            strazi[indiceLinie/3] = currentline;
+            strazi[indiceLinie / 3] = currentline;
         }
-        if (indiceLinie%3==2)
+        if (indiceLinie % 3 == 2)
         {
-            incidente[indiceLinie/3]= atoi(currentline);
+            incidente[indiceLinie / 3] = atoi(currentline);
         }
         indiceLinie++;
         currentline = (char *)malloc(50 * sizeof(char));
-
     }
-    count=indiceLinie-1/3;
+    count = indiceLinie - 1 / 3;
 
     fclose(file);
 
@@ -88,12 +87,14 @@ int main()
             {
                 char index[10];
                 sprintf(index, "%d", i + 1);
-                char descIncident[1000] = "";
+                char
+                    descIncident[1000] = "";
                 strcat(descIncident, index);
                 strcat(descIncident, "-");
                 strcat(descIncident, orase[orasIncidente[i]]);
                 strcat(descIncident, ",");
-                strcat(descIncident, strazi[i]);
+                strcat(descIncident,
+                       strazi[i]);
                 strcat(descIncident, ",");
                 strcat(descIncident, tipIncident[incidente[i]]);
                 printf("%s\n", descIncident);
@@ -102,19 +103,19 @@ int main()
         }
         else if (choice == 2)
         {
-
             int _orasIncident;
-            // Citire oras
+
+           // Citire oras
             printf("Adaugare incident:[1/3] - Selectare oras \n");
-            for (int i = 0; i < nrOrase; i++)
+            for (int i = 0; i < nrOrase;
+                 i++)
             {
                 printf("%d - %s \n", i + 1, orase[i]);
             }
-
             do
             {
                 printf("Introdu varianta dorita: ");
-                scanf("%d", &_orasIncident);
+                scanf(" %d", &_orasIncident);
             } while (_orasIncident < 1 || _orasIncident > nrOrase + 1);
             // citire strada
 
@@ -132,12 +133,45 @@ int main()
             {
                 printf("%d - %s \n", i + 1, tipIncident[i]);
             }
-
             do
             {
                 printf("Introdu varianta dorita: ");
-                scanf("%d", &incident);
+                scanf(" %d", &incident);
+                int choice2;
+                switch (choice2)
+                {
+                case 0:
+                    break;
+                case 1:
+                {
+                    orasIncidente[count];
+                    count++;
+                    FILE *pFile2 = fopen("incidente.txt", "a");
+                    fprintf(pFile2, "Timisoara\n");
+                    fclose(pFile2);
+                    break;
+                }
+                case 2:
+                {
+                    strazi[count];
+                    count++;
+                    FILE *pFile2 = fopen("incidente.txt", "a");
+                    fprintf(pFile2, "Alecsandri\n");
+                    fclose(pFile2);
+                    break;
+                }
+                case 3:
+                {
+                    incidente[count];
+                    count++;
+                    FILE *pFile2 = fopen("incidente.txt", "a");
+                    fprintf(pFile2, "bec defect\n" );
+                    fclose(pFile2);
+                    break;
+                }
+                }
             } while (incident < 1 || incident > nrIncidente + 1);
+
             printf("Raportul a fost adaugat cu succes.\n");
 
             orasIncidente[count] = _orasIncident - 1;
